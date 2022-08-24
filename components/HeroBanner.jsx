f7a4c9a5408d0367da49d98c042f89acd10e5ad7
Jorleidis Monsalvo <jp.monsalvo@uniandes.edu.co>
2022-08-23 22:14:25 -0500

    responsive fix

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import Link from 'next/link'
-import { urlFor } from '../lib/client'
-const HeroBanner = ({ heroBanner}) => {
+import React from "react";
+import Link from "next/link";
+import { urlFor } from "../lib/client";
+const HeroBanner = ({ heroBanner }) => {
   return (
-    <div className='hero-banner-container'>
-        <div className='hero-banner-image'></div>
-        <div>
-            <div className='hero-banner-top'>
-            <h3>{heroBanner.midText}</h3>
-            <h1>{heroBanner.largeText}</h1>
-            </div>
-            <div>
-                <div className='hero-banner-button'>
-                <a href="https://api.whatsapp.com/send/?phone=573123809599&text=Bienvenido+a+OMI+FARM+COLOMBIA&type=phone_number&app_absent=0" target='_blank'  rel="noreferrer">
-                    <button type='button'>{heroBanner.buttonText}</button>
-                </a>
-                </div>
-                <div className='desc'>
-                    <p>{heroBanner.desc}</p>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+    <div className="hero-banner-container">
+      <div className="hero-banner-image"></div>
+      <div className="hero-banner-top">
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText}</h1>
+      </div>
 
-export default HeroBanner
+      <div className="hero-banner-button">
+        <a
+          href="https://api.whatsapp.com/send/?phone=573123809599&text=Bienvenido+a+OMI+FARM+COLOMBIA&type=phone_number&app_absent=0"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">{heroBanner.buttonText}</button>
+        </a>
+      </div>
+
+      <div className="desc">
+        <p>{heroBanner.desc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
-import { Cart } from './'
+import { Cart } from "./";
 import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
-  const { showCart, setShowCart, totalQuantities, changeLine } = useStateContext() 
+  const { showCart, setShowCart, totalQuantities, changeLine } =
+    useStateContext();
   return (
     <div className="navbar-container">
-      <p className="logo">
-        <Link href="/">OMI FARM</Link>
-      </p>
-      <p className="navbar-link">
-        <Link href="/products"><a onClick={()=> changeLine('')}>Productos</a></Link>
-      </p>
-      <p className="navbar-link">
-        <Link href="/testimonios">Testimonios</Link>
-      </p>
-      <p className="navbar-link">
-        <Link href="/aboutUs">Sobre nosotros</Link>
-      </p>
+      <Link href="/">
+        <a className="logo-nav">OMI FARM</a>
+      </Link>
+      <Link href="/products" >
+        <a className="navbar-link" onClick={() => changeLine("")}>Productos</a>
+      </Link>
+      <Link href="/testimonios" className="navbar-link">
+        <a className="navbar-link">Testimonios</a>
+      </Link>
+      <Link href="/aboutUs" className="navbar-link">
+      <a className="navbar-link">Sobre nosotros</a>
+      </Link>
       {/* <button type="button" className="cart-icon" onClick={()=>setShowCart(true)}>
         <AiOutlineShopping />
         <span className="cart-item-qty">{totalQuantities}</span>
